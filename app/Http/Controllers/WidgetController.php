aa77a7f51ac80096f119924f54387a36ac4ebf4d
Ayush Kumar <99477018+Ayush281999@users.noreply.github.com>
2024-09-28 21:19:29 +0530

    deb 2

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -217,8 +217,8 @@ class WidgetController extends Controller
             //     $widgetHtml = 'Please verify your domain';
             //     return response($widgetHtml, 200)->header('Content-Type', 'text/html');
             // }
-            $widgetHtml = view('widget.widget-management', ['widgetData' => $widgetData, 'widgetThemeMode' => $userTheme->light_mode])->render();
             dd($userTheme,$currentDomain);
+            $widgetHtml = view('widget.widget-management', ['widgetData' => $widgetData, 'widgetThemeMode' => $userTheme->light_mode])->render();
         return response($widgetHtml, 200)->header('Content-Type', 'text/html')
                 ->header('X-User-Theme', $userTheme->light_mode);;
         // Render the Blade view to a string
