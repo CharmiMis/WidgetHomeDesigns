5e05f11fe1365634a678e416e75db3323bb2e889
Ayush Kumar <ayushk.itpath@gmail.com>
2025-01-17 14:33:28 +0530

    Remove copmments and fix the issue

--- resources/views/web2/common/widget-design-preview.blade.php
+++ resources/views/web2/common/widget-design-preview.blade.php
@@ -1,72 +1,40 @@
 <!-- Modal -->
-<div class="modal fade gs-image-editing-background" id="modalImagePreview" tabindex="-1" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    {{-- <div class="modal-dialog gs-modal-container">
-        <div class="modal-content gs-modal-content">
+<div class="modal fade gs-image-editing-background" id="modalImagePreview" tabindex="-1"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog  gs-image-editing-container">
+        <div class="modal-content gs-image-editing-content">
             <button type="button" class="gs-modal-close" data-dismiss="modal">
                 <img src="{{ asset('webWidget/images/gs-close-icon.svg') }}">
             </button>
-            {{-- <div class="gs-image-editing-slide-outer">
+            <div class="gs-image-editing-slide-outer">
                 <div class="gs-image-editing-slide">
                     <div class="gs-image-editing-slide-row">
-                        <div class="inked-painted">
-                            <img src="" id="mip_before"
-                                class="inked" alt="results when using beautiful redesign with ai">
-                            <div class="colored" id="mip_after"
-                                style="" title="ai beautiful redesign before generating">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-            <div class="modal-dialog  gs-image-editing-container">
-                <div class="modal-content gs-image-editing-content">
-                    <button type="button" class="gs-modal-close" data-dismiss="modal">
-                        <img src="{{ asset('webWidget/images/gs-close-icon.svg') }}">
-                    </button>
-                    <div class="gs-image-editing-slide-outer">
-                        <div class="gs-image-editing-slide">
-                            <div class="gs-image-editing-slide-row">
-                                <!-- <div class="ba-slide-outer">
-                                    <div class="inked-painted">
+                        <div class="search-preview-outer">
+                            <div class="mainSection">
+                                <div id="one" class="bal-container">
+                                    <div class="bal-after">
                                         <img src="" id="mip_before" class="inked"
                                             alt="results when using beautiful redesign with ai">
-                                        <div class="colored" id="mip_after" style=""
-                                            title="ai beautiful redesign before generating">
-                                        </div>
+                                        <div class="bal-afterPosition afterLabel"> </div>
                                     </div>
-                                </div> -->
-                                <div class="search-preview-outer">
-                                    <div class="mainSection">
-                                        <div id="one" class="bal-container">
-                                            <div class="bal-after">
-                                            <img src="" id="mip_before" class="inked" alt="results when using beautiful redesign with ai">
-                                                <div class="bal-afterPosition afterLabel"> </div>
+                                    <div class="bal-before-outer">
+                                        <div class="bal-before">
+                                            <div class="bal-before-inset"> <img id="mip_after"
+                                                    alt="ai beautiful redesign before generating">
+                                                <div class="bal-beforePosition beforeLabel"> </div>
                                             </div>
-                                            <div class="bal-before-outer">
-                                                <div class="bal-before">
-                                                    <div class="bal-before-inset"> <img id="mip_after" alt="ai beautiful redesign before generating">
-                                                        <div class="bal-beforePosition beforeLabel"> </div>
-                                                    </div>
-                                                </div>
-                                            <div class="bal-handle left-right-arrow-slider"> <span class=" handle-left-arrow"></span> <span
-                                                    class="handle-right-arrow"></span> </div>
-                                            </div>                                            
                                         </div>
+                                        <div class="bal-handle left-right-arrow-slider"> <span
+                                                class=" handle-left-arrow"></span> <span
+                                                class="handle-right-arrow"></span> </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-                        {{-- <div class="btn-wrap">
-                            <button class="prev-btn"><span><img
-                                        src="{{ asset('webWidget/images/gs-slide-arrow.svg') }}"></span>Prev</button>
-                            <button class="next-btn"><span><img
-                                        src="{{ asset('webWidget/images/gs-slide-arrow.svg') }}"></span>Next</button>
-                        </div> --}}
+
                     </div>
                 </div>
             </div>
-        {{-- </div>
-    </div> --}}
+        </div>
+    </div>
 </div>
